675bc8976c5b28d85743c26be31a23b776146aca
ChoiYoungMin <com5090@naver.com>
2016-04-04 22:17:34 +0900

    gg

--- app/src/main/java/com/index/projectsns/Activity_Login.java
+++ app/src/main/java/com/index/projectsns/Activity_Login.java
@@ -1,14 +1,42 @@
 package com.index.projectsns;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tsengvn.typekit.TypekitContextWrapper;
 
 public class Activity_Login extends AppCompatActivity {
+
+    Button btn;
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__login);
+        setContentView(R.layout.activity_login);
 
+        init();
+
+    }
+
+    public void init() {
+        btn = (Button)findViewById(R.id.Login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Activity_Login.this,Activity_main.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
